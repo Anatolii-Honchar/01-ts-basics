8044dfc10acfdf556778e88e22aec00957e6e888
Anatolii Honchar <gonchar_tol@ukr.net>
2026-02-20 21:01:48 +0200

    Add type annotations for arrays and define Product interface in task-3; make getFirstElement generic in task-6

--- src/task-6.ts
+++ src/task-6.ts
@@ -1,10 +1,10 @@
-function getFirstElement(arr) {
+function getFirstElement<T>(arr: T[]): T {
   return arr[0];
 }
 
-getFirstElement([1, 2, 3]); // 1
-getFirstElement(["a", "b", "c"]); // "a"
-getFirstElement([true, false, true]); // true
+getFirstElement<number>([1, 2, 3]); // 1
+getFirstElement<string>(["a", "b", "c"]); // "a"
+getFirstElement<boolean>([true, false, true]); // true
 
 /*Завдання:
 Зроби функцію узагальненою, використовуючи тип T, щоб вона працювала з масивами будь-якого типу.
